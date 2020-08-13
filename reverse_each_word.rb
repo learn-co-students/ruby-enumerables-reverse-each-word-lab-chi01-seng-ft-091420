f8c30986e6343beef618bbd33d38412e5fe6e2da
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-13 18:42:18 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   string_array = string.split
-  reverse_array = string_array.collect{ |i| i.reverse }
-  reverse_array.join(' ')
+  string_array.collect{|i| i.reverse}.join(' ')
 end
   
   reverse_each_word("hello my name is Kyle")
