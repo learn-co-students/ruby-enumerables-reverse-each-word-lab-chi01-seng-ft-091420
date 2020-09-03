59cb08e5bada2af23fc700eccfa76516a4d74971
John Lovitsch <jmlovitsch@gmail.com>
2020-09-03 14:03:56 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_array(string)
+  array1 = string.split(" ")
+  array2 = []
+  
+  array1.each do |string|
+    array2 << string.reverse
+  end
+  array2
+end
+
+def reverse_each_word(string)
+  array1 = string.split(" ")
+  array2 = []
+  array1.collect do|string|
+    array2 << string.reverse
+  end
+  array2.join(" ")
+end
+  
