339b286347b3abdc81173ca385c515fe6ebeea39
Tyler Wallace <tywallace2017@gmail.com>
2020-09-08 22:37:05 +0000

    Done.

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -2,8 +2,8 @@
 
 
 array = {
-:sentence1 = { "Hello there, and how are you?" }, 
-:sentence2 = { "Hi again, just making sure it's reversed!" }
+:sentence1 => { "Hello there, and how are you?" }, 
+:sentence2 => { "Hi again, just making sure it's reversed!" }
 }
 p reverse_each_word[:sentence1]
 p reverse_each_word[:sentence2]
